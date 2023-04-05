dfa5d0a5462a238b74dc40b2a6dc012f45f51933
joonpiter <75423505+joonpiter@users.noreply.github.com>
2023-04-04 19:54:54 -0700

    new changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,16 @@
-import { PrettyChatWindow } from "react-chat-engine-pretty";
+import { useState } from "react";
 
-const ChatsPage = () => {
-  return (
-    <div style={{ height: "100vh", width: "100vw" }}>
-      <PrettyChatWindow
-        projectId={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID}
-        username="zack123" // adam
-        secret="pass1234" // pass1234
-        style={{ height: "100%" }}
-      />
-    </div>
-  );
-};
+import AuthPage from "./authPage";
+import ChatsPage from "./chatsPage";
 
-export default ChatsPage;
+function App() {
+  const [user, setUser] = useState();
+
+  if (!user) {
+    return <AuthPage onAuth={(user) => setUser(user)} />;
+  } else {
+    return <ChatsPage user={user} />;
+  }
+}
+
+export default App;
